c6191d33dab6aa196796b92204b747e1e85e0d2d
amber-tamara <ambertjansen@gmail.com>
2020-05-07 13:01:44 +0100

    form message and email

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,7 +9,9 @@ const Contact = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <input name="name" placeholder="Your Name..." type="text" />
+        <input name="name" placeholder="Your Name" type="text" />
+        <input name="email" placeholder="name@name.com" type="email" />
+        <textarea name="message" />
         <button>Send</button>
       </form>
     </div>

3dbb50b33102b3fa5ea47b7664fb67007edab597
amber-tamara <ambertjansen@gmail.com>
2020-05-07 12:49:01 +0100

    form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,15 +2,17 @@ import React from "react"
 
 const Contact = () => {
   return (
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input name="name" placeholder="Your Name..." type="text" />
-      <button>Send</button>
-    </form>
+    <div>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input name="name" placeholder="Your Name..." type="text" />
+        <button>Send</button>
+      </form>
+    </div>
   )
 }
 

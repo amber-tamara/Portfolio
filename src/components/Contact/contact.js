5b90446edf29815f289605c2c44d838344bf21c2
amber-tamara <ambertjansen@gmail.com>
2020-05-12 21:25:17 +0100

    form update

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,7 +3,7 @@ import contact from "./contact.module.css"
 
 const Contact = () => {
   return (
-    <div>
+    <div className="body">
       <h2 className={contact.title}>GET IN TOUCH!</h2>
       <ul className={contact.details}>
         <li>0777111556</li>
@@ -16,11 +16,8 @@ const Contact = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <label>FULL NAME*</label>
         <input name="name" placeholder="Your Full Name" type="text" />
-        <label>EMAIL ADRESS*</label>
         <input name="email" placeholder="Email address" type="email" />
-        <label>MESSAGE*</label>
         <textarea name="message" placeholder="Message" rows="5" />
         <button>Send</button>
       </form>

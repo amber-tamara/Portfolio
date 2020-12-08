e8a71e9827c54c2c9d25f39200d40273fdaec243
amber-tamara <ambertjansen@gmail.com>
2020-12-08 00:54:07 +0000

    Changed images and made them responsive

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,88 +1,78 @@
 import React from "react"
-import projects from "./project.module.css"
+import "./project.css"
 
 const Projects = () => {
   return (
-    <div className={projects.background}>
-      <div className={projects.container}>
-        <div className={projects.mainTitleContainer}>
-          <h2>My Work</h2>
-          <div className={projects.line}></div>
+    <div className="projects--background">
+      <div className="projects--box">
+        <div className="header--box">
+          <h2 className="heading">My Work</h2>
+          <div className="header__underline"></div>
         </div>
-        <div className={projects.project}>
-          <div className={projects.screenShots}>
-            <img className={projects.imgMd} src={require("./images/ArtWebsite/tabletArtSite.png")}></img>
-            <img className={projects.imgSm} src={require("./images/ArtWebsite/mobileArtSite.png")}></img>
-          </div>
-          <div className={projects.contentContainer}>
-            <div className={projects.titleContainer}>
+        <div className="projects first--project">
+          <div className="project--wrapper">
+            <div className="project--images artsite--images">
+              <img className="artimg__md" src={require("./images/ArtWebsite/tabbby.png")}></img>
+              <img className="artimg__sm" src={require("./images/ArtWebsite/mobileArtSite.png")}></img>
+            </div>
+            <div className="content--container">
               <h2>ANNETTE'S ART</h2>
+              <p className="content__paragraph">orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <div className="content__buttons">
+                <a>DEMO</a>
+                <a>CODE</a>
+              </div>
+              <ul className="content__tools">
+                <li>GATSBY</li>
+                <li>REACT</li>
+                <li>SCSS</li>
+              </ul>
             </div>
-            <p className={projects.paragraph}>Built with Gatsby which uses ReactJS components.
-            Performance isn’t optional, it's built in, with
-            improved loading time by code splitting. Intelligent
-          image loading</p>
-            <div className={projects.descriptionButtons}>
-              <a>DEMO</a>
-              <a>CODE</a>
-            </div>
-            <ul className={projects.usedItems}>
-              <li>GATSBY</li>
-              <li>REACT</li>
-              <li>SCSS</li>
-            </ul>
           </div>
         </div>
-        <div className={projects.lineSpace}></div>
-        <div className={projects.project}>
-          <div className={projects.screenShotsWeather}>
-            <img className={projects.imgMd} src={require("./images/WeatherSite/tabletWeatherSite.png")}></img>
-            <img className={projects.imgSm} src={require("./images/WeatherSite/mobileWeatherSite.png")}></img>
-          </div>
-          <div className={projects.contentContainer}>
-            <div className={projects.titleContainer}>
+        <div className="projects second--project">
+          <div className="project--wrapper">
+            <div className="project--images weathersite--images">
+              <img className="img__md" src={require("./images/WeatherSite/tabi.png")}></img>
+              <img className="img__sm" src={require("./images/WeatherSite/mob.png")}></img>
+            </div>
+            <div className="content--container">
               <h2>WEATHER APP</h2>
+              <p className="content__paragraph">orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <div className="content__buttons">
+                <a>DEMO</a>
+                <a>CODE</a>
+              </div>
+              <ul className="content__tools">
+                <li>HTML</li>
+                <li>JAVASCRIPT</li>
+                <li>SCSS</li>
+                <li>OPENWEATHER API</li>
+              </ul>
             </div>
-            <p className={projects.paragraph}>Built with Gatsby which uses ReactJS components.
-            Performance isn’t optional, it's built in, with
-            improved loading time by code splitting. Intelligent
-          image loading</p>
-            <div className={projects.descriptionButtons}>
-              <a>DEMO</a>
-              <a>CODE</a>
-            </div>
-            <ul className={projects.usedItems}>
-              <li>HTML</li>
-              <li>JAVASCRIPT</li>
-              <li>SCSS</li>
-              <li>OPENWEATHER API</li>
-            </ul>
           </div>
         </div>
-        <div className={projects.lineSpace}></div>
-        <div className={projects.thirdProject}>
-          <div className={projects.screenShots}>
-            <img className={projects.imgMd} src={require("./images/GiphyWebsite/tabletGiphySite.png")}></img>
-            <img className={projects.imgSm} src={require("./images/GiphyWebsite/mobileGiphySite.png")}></img>
-          </div>
-          <div className={projects.contentContainer}>
-            <div className={projects.titleContainer}>
+
+        <div className="projects third--project">
+          <div className="project--wrapper">
+            <div className="project--images giphysite--images">
+              <img className="img__md" src={require("./images/GiphyWebsite/gif.png")}></img>
+              <img className="img__sm" src={require("./images/GiphyWebsite/cute.png")}></img>
+            </div>
+            <div className="content--container">
               <h2>GIFSPOT</h2>
+              <p className="content__paragraph">orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <div className="content__buttons">
+                <a>DEMO</a>
+                <a>CODE</a>
+              </div>
+              <ul className="content__tools">
+                <li>HTML</li>
+                <li>JAVASCRIPT</li>
+                <li>SCSS</li>
+                <li>GIPHY API</li>
+              </ul>
             </div>
-            <p className={projects.paragraph}>Built with Gatsby which uses ReactJS components.
-            Performance isn’t optional, it's built in, with
-            improved loading time by code splitting. Intelligent
-          image loading</p>
-            <div className={projects.descriptionButtons}>
-              <a>DEMO</a>
-              <a>CODE</a>
-            </div>
-            <ul className={projects.usedItems}>
-              <li>HTML</li>
-              <li>JAVASCRIPT</li>
-              <li>SCSS</li>
-              <li>GIPHY API</li>
-            </ul>
           </div>
         </div>
       </div>

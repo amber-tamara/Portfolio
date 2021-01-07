e438501b003bc0dc3265a2e77ef1b6847c5ab20e
amber-tamara <ambertjansen@gmail.com>
2021-01-07 20:47:19 +0000

    remove styled components and replaced with th css

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -1,129 +1,33 @@
 import { FaBars } from 'react-icons/fa';
-import styled from 'styled-components';
-import React from 'react';
-
-export const Nav = styled.nav`
-    background-color: ${({ navbar }) => (navbar ? 'rgb(7, 7, 7);' : 'transparent')};
-    height: ${({ navbar }) => (navbar ? '50px;' : '80px')};
-    transition: ${({ navbar }) => (navbar ? '.5s ease-in-out, padding .5s ease-in-out' : '')};
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    position: ${({ navbar }) => (navbar ? 'fixed' : 'absolute')};
-    top: 0;
-    z-index: 10;
-    font-family: "Raleway",monospace;
-    transition: all .3s ease;
-    font-size: 1.1rem;
-    z-index: 999;
-    @media screen and (max-width: 760px){
-     height: 80px;
-    }
-    @media screen and (max-width: 960px) {
-        transition: 0.8s all ease;
-    }
-`
-
-export const NavbarContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    height: 80px;
-    z-index: 1;
-    width: 100%;
-    padding: 0 24px;
-    max-width: 1100px;
-`
-
-export const NavLogo = styled.h2`
-    visibility:  ${({ navbar }) => (navbar ? 'visible' : 'hidden')};
-    transition: all .3s ease;
-    opacity: 100%;
-    color: #5DE189;
-    font-size: 1.5rem;
-    align-items: center;
-    display: flex;
-    font-weight: bold;
-    text-decoration: none;
-    margin: 0;
-    font-family: "Nunito";
-`
-
-export const MobileIcon = styled.div`
-    display: none;
-    @media screen and (max-width: 760px){
-        display: block;
-        position: absolute;
-        top: 4px;
-        transition: .5s ease-in-out, padding .5s ease-in-out;
-        right: 0;
-        transform: translate(-100%, 60%);
-        font-size: 1.8rem;
-        cursor: pointer;
-        color: #f2f2f2;
-    }
-`
-
-export const NavMenu = styled.ul`
-    margin-bottom: 0;
-    display: flex;
-    align-items: center;
-    list-style: none;
-    text-align: center;
-    padding-right: 30px;
-    margin-right: -22px;   
-    @media screen and (max-width: 760px){
-        display: none;
-    }
-`
-
-export const NavItem = styled.li`
-    height: 80px;
-`
-
-export const NavLinks = styled.a`
-    outline: none;
-    color: #f2f2f2; 
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding-right: 1.4em;
-    height: 100%;
-    cursor: pointer;
-    transition: all .3s ease;
-    &:hover {
-        color: #5DE189;
-    }
-`
+import React from 'react'
+import "./NavbarElements.css"
 
 const Index = ({ toggle, navbar }) => {
+
     return (
-        <Nav navbar={navbar}>
-            <NavbarContainer>
-                <NavLogo navbar={navbar}>Amber Jansen</NavLogo>
-                <MobileIcon navbar={navbar} onClick={toggle}>
+        <div className={`nav ${navbar ? 'navVisible' : ''}`}>
+            <div className={'navbarContainer'}>
+                <h3 className={`${navbar ? 'navLogo' : 'navLogoInvisible'}`} navbar={navbar}>Amber Jansen</h3>
+                <div className={'mobileIcon'} onClick={toggle} onKeyDown={toggle} aria-hidden="true">
                     <FaBars />
-                </MobileIcon>
-                <NavMenu>
-                    <NavItem>
-                        <NavLinks href="#home">Home</NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks href="#about">About</NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks href="#work">My Work</NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks href="#contact">Contact</NavLinks>
-                    </NavItem>
-                </NavMenu>
-            </NavbarContainer>
-        </Nav>
+                </div>
+                <ul className={'navMenu'}>
+                    <li className={'navItem'}>
+                        <span className={'navLinks'} to="home">Home</span>
+                    </li>
+                    <li className={'navItem'}>
+                        <span className={'navLinks'} to="about">About</span>
+                    </li>
+                    <li className={'navItem'}>
+                        <span className={'navLinks'} to="projects">My Work</span>
+                    </li>
+                    <li className={'navItem'}>
+                        <span className={'navLinks'} to="contact">Contact</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
 export default Index
-
-
--- src/components/Header/Sidebar/index.js
+++ src/components/Header/Sidebar/index.js
@@ -1,78 +1,8 @@
-import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-import styled from 'styled-components'
-import { FaTimes } from 'react-icons/fa'
-
-export const SidebarContainer = styled.aside`
-  height: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-  opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-  top: ${({ isOpen }) => (isOpen ? '0' : '-100')};    
-  @media screen and (max-width: 760px){
-  z-index: 999;
-  width: 100%;
-  position: fixed;
-  background: black;
-  display: flex;
-  justify-content: center;
-  top: 0;
-  left: 0;
-  transition: 0.3s ease-in-out;
-  font-family: "Raleway",monospace;
-  }
-`
-
-export const CloseIcon = styled(FaTimes)`
-  color: #f2f2f2;
-  transition: 0.2s ease-in-out;
-  &:hover {
-    color: #5DE189;
-  }
-`
-
-export const Icon = styled.div`
-  position: absolute;
-  top: 1.4rem;
-  right: 1.5rem;
-  background: transparent;
-  font-size: 2rem;
-  cursor: pointer;
-  outline: none;
-`
-
-export const SidebarWrapper = styled.div`
-  color: #f2f2f2;
-`
-
-export const SidebarMenu = styled.ul`
-  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
-  justify-content: center;
-  flex-direction: column;
-  padding: 0;
-
-  @media screen and (max-width:  480px) {
-      grid-template-rows: repeat(6, 60px);
-  }
-`
-
-export const SidebarLink = styled.a`
-  display: flex;
-  padding: 40px;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.5rem;
-  text-decoration: none;
-  list-style: none;
-  transition: 0.2s ease-in-out;
-  text-decoration: none;
-  color: #f2f2f2;
-  cursor: pointer;
-
-  &:hover {
-    color: #5DE189;
-    transition: 0.2s ease-in-out;
-}
-`
+import React from 'react';
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import { FaTimes } from 'react-icons/fa';
+import "./SidebarElement.css";
 
 const SidebarElement = ({ toggle, isOpen }) => {
     const data = useStaticQuery(graphql`
@@ -87,41 +17,36 @@ const SidebarElement = ({ toggle, isOpen }) => {
             }
       `)
 
-    const svgStyle = {
-        width: '70px',
-        height: '70px',
-        margin: '40px auto',
-    };
     return (
-        <SidebarContainer isOpen={isOpen}>
-            <Icon onClick={toggle} >
-                <CloseIcon />
-            </Icon>
-            <SidebarWrapper>
-                <SidebarMenu isOpen={isOpen}>
-                    <SidebarLink href="#home" onClick={toggle}>
-                        Home
-                </SidebarLink>
-                    <SidebarLink href="#about" onClick={toggle}>
-                        About
-                </SidebarLink>
-                    <SidebarLink href="#work" onClick={toggle}>
-                        My Work
-                </SidebarLink>
-                    <SidebarLink href="#contact" onClick={toggle}>
-                        Contact Me
-                </SidebarLink>
+        <div className={`sidebarContainer ${isOpen ? 'sidebarContainerVisible' : 'sidebarContainerInvisible'}`} onKeyDown={isOpen} aria-hidden="true">
+            <div className={'icon'} onClick={toggle} onKeyDown={toggle} aria-hidden="true">
+                <FaTimes className={'closeIcon'} />
+            </div>
+            <div className={'SidebarWrapper'}>
+                <ul className={`${isOpen ? 'sidebarContainerVisible' : '.sidebarContainerInvisible'}`} onKeyDown={isOpen} aria-hidden="true">
+                    <li className={'sidebarLink'}>
+                        <a href='#home' onClick={toggle} onKeyDown={toggle}> Home </a>
+                    </li>
+                    <li className={'sidebarLink'}>
+                        <a href='#about' onClick={toggle} onKeyDown={toggle}> About </a>
+                    </li>
+                    <li className={'sidebarLink'}>
+                        <a href='#work' onClick={toggle} onKeyDown={toggle}> My Work </a>
+                    </li>
+                    <li className={'sidebarLink'}>
+                        <a href='#contact' onClick={toggle} onKeyDown={toggle}> Contact Me </a>
+                    </li>
                     <a
-                        style={svgStyle}
+                        className={'svgStyle'}
                         href="https://github.com/amber-tamara"
                         target="_blanc"
                         rel="noopener noreferrer"
                     >
                         <Img fluid={data.file.childImageSharp.fluid} alt="Github Icon" />
                     </a>
-                </SidebarMenu>
-            </SidebarWrapper>
-        </SidebarContainer>
+                </ul>
+            </div>
+        </div>
     )
 }
 
